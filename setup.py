df8755dbf7417f2d6268f213e5aef8344d85a82a
Mike Jefferies <msj@pointwise.com>
2016-11-11 15:58:39 -0600

    Moving up the version number since pypi was detecting an previous file that used version 1.0.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
         return f.read()
 
 setup(name='pointwise-glyph-client',
-      version='1.0.1',
+      version='1.0.2',
       description='Python client to Pointwise Glyph Server',
       url='http://github.com/pointwise/GlyphClientPython',
       packages=['pointwise'])
