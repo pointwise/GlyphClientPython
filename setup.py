947412df0f6dff1b980baec64da5227d1c4e8b83
Patrick Baker <pbaker@pointwise.com>
2019-03-15 10:33:05 -0500

    Update setup.py to 2.0.2 for version compatability

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
         return f.read()
 
 setup(name='pointwise-glyph-client',
-      version='2.0.1',
+      version='2.0.2',
       description='Glyph client in Python with Python-like API to Pointwise Glyph Server',
       url='http://github.com/pointwise/GlyphClientPython',
       install_requires=['numpy'],
